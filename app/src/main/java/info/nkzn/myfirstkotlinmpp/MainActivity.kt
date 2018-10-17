e89f7749c69a3c06fbf95bb4fec1c4f5db2142e5
Yukiya Nakagawa <yn.airscope@gmail.com>
2018-10-17 19:42:53 +0900

    feat(android): Android app works

--- app/src/main/java/info/nkzn/myfirstkotlinmpp/MainActivity.kt
+++ app/src/main/java/info/nkzn/myfirstkotlinmpp/MainActivity.kt
@@ -2,11 +2,15 @@ package info.nkzn.myfirstkotlinmpp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import org.kotlin.mpp.mobile.createApplicationScreenMessage
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.main_text).text = createApplicationScreenMessage()
     }
 }
